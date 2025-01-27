11ee4be8a2db8638ee37896003d03f22a9cf7ad2
NicoGGomez <anelecarg@icloud.com>
2025-01-27 17:48:06 -0300

    tres en raya terminado (local storage)

--- projects/02-tres-en-raya/src/App.jsx
+++ projects/02-tres-en-raya/src/App.jsx
@@ -9,10 +9,16 @@ import { WinnerModal } from "./components/WinnerModal"
 function App() {
   // DEFINE EL ESTADO INICIAL DEL TABLERO CON 
   // ESPACIOS EN BLANCO 
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
 
   // DEFINE QUIEN VA A EMPEZAR A JUGAR
-  const [turn, setTurn] = useState(TURNS.X) 
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
   // DEFINE ESTADO DE QUIEN GANA
   const [winner, setWinner] = useState(null)
@@ -21,6 +27,9 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   // FUNCION PARA ACTUALIZAR EL TURNO DEL TABLERO, LE PASAMOS UN 
@@ -36,6 +45,10 @@ function App() {
     // OPERADOR TERNARIO (SI EL TURNO ES IGUAL A X, ENTONCES O, SINO X) 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X 
     setTurn(newTurn)
+
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+
     // REVISAR SI HAY UN GANADOR 
     const newWinner = checkWinnerFrom(newBoard)
     if(newWinner){
